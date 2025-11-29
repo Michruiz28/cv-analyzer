28622c8d97c2fe8b862a0aefcb18416a708d2ff3
Ch0comilo <danielipe17mh@gmail.com>
2025-11-29 12:45:20 -0500

    configurando frontend

---
+++ app/Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="CV Analyzer",
+    page_icon="📄",
+)
+
+st.title("📄 CV Analyzer")
+st.write("Bienvenido a la aplicación. Esta es la página principal.")
